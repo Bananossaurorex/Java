d55e58211d603e3fd01c2a9521ec03928825d29c
Bananossaurorex <160606050+Bananossaurorex@users.noreply.github.com>
2025-04-08 11:02:38 -0300

    Delete AtividadePontuadaVerdade/AtividadePontuadaVerdade directory

--- AtividadePontuadaVerdade/AtividadePontuadaVerdade/src/main/java/com/senai/AtividadePontuadaVerdade/exception/EmailJaCadastradoException.java
+++
@@ -1,6 +0,0 @@
-package com.senai.AtividadePontuadaVerdade.exception;
-
-public class EmailJaCadastradoException extends RuntimeException {
-    public EmailJaCadastradoException(String message) {
-    }
-}
